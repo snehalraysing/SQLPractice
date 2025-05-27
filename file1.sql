0154fa72b61deac66ae0166be7e094f28b4b6587
Snehal Raysing <96164672+snehalraysing@users.noreply.github.com>
2025-05-27 12:58:09 +0530

    Update file1.sql

--- file1.sql
+++ file1.sql
@@ -15,3 +15,29 @@ where (state='CA' or state='NY') and shirt_or_hat='shirt';
 
 select first_name, last_name, state
 from people where state like 'C%';
+
+select first_name, last_name, state
+from people where first_name like '%o%';
+
+select first_name, last_name, state, company
+from people where company like '%LLC'
+limit 10;
+
+select first_name, last_name, state, company
+from people where company like '%LLC'
+limit 10 offset 4;
+
+-- ORDER BY
+select first_name , last_name from people 
+order by first_name;  -- by default asc
+
+select first_name , last_name from people 
+order by first_name desc; 
+
+select state, last_name, first_name from people
+order by state asc, last_name desc;
+
+select first_name, length(first_name) from people;
+
+-- distinct 
+select distinct(first_name) from people;
